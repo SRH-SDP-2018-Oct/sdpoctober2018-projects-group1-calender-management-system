274ba0ea57646cd55a61e0e4a4f3ae6af10c77c3
rajkiran <rajkiran.one@gmail.com>
2018-12-11 22:43:31 +0100

    removed extra spaces in createfolder

--- src/main/java/srh/Calendarapp/CreateFolder.java
+++ src/main/java/srh/Calendarapp/CreateFolder.java
@@ -15,8 +15,6 @@ public class CreateFolder {
 	private static String taskName;
 	Scanner in = new Scanner(System.in);
 	int eventNumber;
-
-
 	public void gotoFolder(){
 
 		try {    
@@ -24,34 +22,23 @@ public class CreateFolder {
 			Class.forName("com.mysql.cj.jdbc.Driver");      
 			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/calender","root","root"); 
 			stmt = conn.createStatement();
-
 			System.out.println("Enter \n1.Create your own Folder\n2.To Exit");
-
 			eventNumber = in.nextInt();
 			if(eventNumber == 1) {
-
 				System.out.print("Enter your folder name \n");
-
 				foName = in.next();
 				System.out.println("Folder name :"+ " "+foName); 
-
 				System.out.print("Enter \n1.Add event to this folder\n2. To show Events");
-
 				eventNumber = in.nextInt();
-
 				if(eventNumber == 1) {
-
 					System.out.print("Enter your Event name \n");    
-
 					taskName = in.next();
-
 					PreparedStatement stmt3=conn.prepareStatement("insert into folder_names(f_name,event_name) values(?,?)");      
 					stmt3.setString(1,foName);
 					stmt3.setString(2,taskName);
 					stmt3.execute();
 					System.out.println("--------------------");  
 					System.out.println(taskName + " Event is added to the Folder: "+ foName); 
-
 					System.out.println("--------------------");  
 					System.out.println("Folder Successfully Created");  
 					System.out.println("--------------------\n");
